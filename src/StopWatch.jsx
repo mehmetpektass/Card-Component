d6cdf1c90d105b4d661d35a4a3ce9d2a15d4467a
mehmetpektass <pektasmehmett1184@gmail.com>
2024-04-05 10:58:27 +0300

    created stopwatch component and it's content

--- src/StopWatch.jsx
+++ src/StopWatch.jsx
@@ -1,6 +1,35 @@
-
+import React,{useState , useEffect , useRef} from "react"
 
 function StopWatch(){
+
+    const [isRunning, setIsRunning] = useState(null);
+    const [elapsedTime , setElapedTime] = useState(0);
+    const intervalIdRef = useRef(null);
+    const startTimeRef = useRef(0);
+
+    const start = () =>{
+
+    }
+    const stop = () =>{
+
+    }
+    const reset = () =>{
+
+    }
+    const formatTime = () =>{
+
+    }
+
+    return(
+        <div className="stopwatch">
+            <div className="display">{formatTime()}</div>
+            <div className="control">
+                <button className="start-button" onClick={start}>Start</button>
+                <button className="stop-button" onClick={stop}>Stop</button>
+                <button className="reset-button" onClick={reset}>Reset</button>
+            </div>
+        </div>
+    );
 
 }
 export default StopWatch
