80a9e63e04fa31f29553b6e98bb67ceadac0ee42
mehmetpektass <pektasmehmett1184@gmail.com>
2024-04-05 14:49:12 +0300

    edited StopWatch.jsx

--- src/StopWatch.jsx
+++ src/StopWatch.jsx
@@ -7,18 +7,46 @@ function StopWatch(){
     const intervalIdRef = useRef(null);
     const startTimeRef = useRef(0);
 
-    const start = () =>{
+    useEffect(() =>{
 
+        if (isRunning) {
+            intervalIdRef.current = setInterval(() => {
+                setElapedTime(Date.now() - startTimeRef.current)
+            }, 10);
+        }
+        
+        return () =>{
+            clearInterval(intervalIdRef.current);
+        }
+
+    },[isRunning])
+
+    const start = () =>{
+        setIsRunning(true);
+        startTimeRef.current = Date.now()-elapsedTime ;
+        console.log(startTimeRef);
     }
     const stop = () =>{
-
+        setIsRunning(false);
     }
     const reset = () =>{
-
+        setElapedTime(0);
+        setIsRunning(false);
     }
     const formatTime = () =>{
-        return "00:00:00"
+
+        let minutes = Math.floor(elapsedTime / (1000 * 60) %60)
+        let seconds = Math.floor(elapsedTime / (1000) %60)
+        let milliseconds = Math.floor((elapsedTime % 1000)/10)
+
+        minutes = String(minutes).padStart(2, "0");
+        seconds = String(seconds).padStart(2, "0");
+        milliseconds = String(milliseconds).padStart(2, "0");
+
+
+        return `${minutes}:${seconds}:${milliseconds}`
     }
+    
 
     return(
         <div className="stopwatch">
